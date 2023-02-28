012d14dc20a0bdf3bd1435eb356cf0f6d1ee5899
Artrios <dapaquelet@hotmail.com>
2023-02-28 20:07:39 +0800

    Fix dexnav pokemon icon palettes

--- src/pokemon_icon.c
+++ src/pokemon_icon.c
@@ -2870,7 +2870,7 @@ void TryLoadAllMonIconPalettesAtOffset(u16 offset)
     s32 i;
     const struct SpritePalette* monIconPalettePtr;
 
-    if (offset <= BG_PLTT_ID(10))
+    if (offset <= OBJ_PLTT_ID(10))
     {
         monIconPalettePtr = gMonIconPaletteTable;
         for (i = ARRAY_COUNT(gMonIconPaletteTable) - 1; i >= 0; i--)
