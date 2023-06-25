5f4b3bd619c2ac1e910cedab278452f22fe19ec5
Eemeliri <eemeli.rintanen@gmail.com>
2023-06-25 11:53:38 +0300

    re-implement object cloning frmo pret

--- include/global.fieldmap.h
+++ include/global.fieldmap.h
@@ -67,7 +67,7 @@ struct BackupMapLayout
 struct ObjectEventTemplate
 {
     /*0x00*/ u8 localId;
-    ///*0x01*/ u8 inConnection; // Leftover from FRLG
+    /*0x01*/ u8 kind; // Always OBJ_KIND_NORMAL in Emerald.
     /*0x02*/ u16 graphicsId;
     /*0x04*/ s16 x;
     /*0x06*/ s16 y;
