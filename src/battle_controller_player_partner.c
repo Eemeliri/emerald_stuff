fd2805652e0952cd48964046dd52e29bccb94003
DizzyEggg <jajkodizzy@wp.pl>
2023-08-08 12:55:47 +0200

    improve switching with ai bad moves

--- src/battle_controller_player_partner.c
+++ src/battle_controller_player_partner.c
@@ -352,23 +352,30 @@ static void PlayerPartnerHandleChooseMove(u32 battler)
     chosenMoveId = gBattleStruct->aiMoveOrAction[battler];
     gBattlerTarget = gBattleStruct->aiChosenTarget[battler];
 
-    if (gBattleMoves[moveInfo->moves[chosenMoveId]].target & (MOVE_TARGET_USER | MOVE_TARGET_USER_OR_SELECTED))
-        gBattlerTarget = battler;
-    if (gBattleMoves[moveInfo->moves[chosenMoveId]].target & MOVE_TARGET_BOTH)
+    if (chosenMoveId == AI_CHOICE_SWITCH)
     {
-        gBattlerTarget = GetBattlerAtPosition(B_POSITION_OPPONENT_LEFT);
-        if (gAbsentBattlerFlags & gBitTable[gBattlerTarget])
-            gBattlerTarget = GetBattlerAtPosition(B_POSITION_OPPONENT_RIGHT);
+        BtlController_EmitTwoReturnValues(BUFFER_B, 10, 0xFFFF);
     }
-
-    if (ShouldUseZMove(battler, gBattlerTarget, moveInfo->moves[chosenMoveId]))
-        QueueZMove(battler, moveInfo->moves[chosenMoveId]);
-
-    // If partner can mega evolve, do it.
-    if (CanMegaEvolve(battler))
-        BtlController_EmitTwoReturnValues(BUFFER_B, 10, (chosenMoveId) | (RET_MEGA_EVOLUTION) | (gBattlerTarget << 8));
     else
-        BtlController_EmitTwoReturnValues(BUFFER_B, 10, (chosenMoveId) | (gBattlerTarget << 8));
+    {
+        if (gBattleMoves[moveInfo->moves[chosenMoveId]].target & (MOVE_TARGET_USER | MOVE_TARGET_USER_OR_SELECTED))
+            gBattlerTarget = gActiveBattler;
+        if (gBattleMoves[moveInfo->moves[chosenMoveId]].target & MOVE_TARGET_BOTH)
+        {
+            gBattlerTarget = GetBattlerAtPosition(B_POSITION_OPPONENT_LEFT);
+            if (gAbsentBattlerFlags & gBitTable[gBattlerTarget])
+                gBattlerTarget = GetBattlerAtPosition(B_POSITION_OPPONENT_RIGHT);
+        }
+
+        if (ShouldUseZMove(gActiveBattler, gBattlerTarget, moveInfo->moves[chosenMoveId]))
+            QueueZMove(gActiveBattler, moveInfo->moves[chosenMoveId]);
+
+        // If partner can mega evolve, do it.
+        if (CanMegaEvolve(gActiveBattler))
+            BtlController_EmitTwoReturnValues(BUFFER_B, 10, (chosenMoveId) | (RET_MEGA_EVOLUTION) | (gBattlerTarget << 8));
+        else
+            BtlController_EmitTwoReturnValues(BUFFER_B, 10, (chosenMoveId) | (gBattlerTarget << 8));
+    }
 
     PlayerPartnerBufferExecCompleted(battler);
 }
@@ -382,7 +389,7 @@ static void PlayerPartnerHandleChoosePokemon(u32 battler)
         chosenMonId = gSelectedMonPartyId = GetFirstFaintedPartyIndex(battler);
     }
     // Switching out
-    else
+    else if (gBattleStruct->monToSwitchIntoId[gActiveBattler] == PARTY_SIZE)
     {
         chosenMonId = GetMostSuitableMonToSwitchInto();
         if (chosenMonId == PARTY_SIZE) // just switch to the next mon
@@ -401,6 +408,12 @@ static void PlayerPartnerHandleChoosePokemon(u32 battler)
             }
         }
         *(gBattleStruct->monToSwitchIntoId + battler) = chosenMonId;
+    }
+    else // Mon to switch out has been already chosen.
+    {
+        chosenMonId = gBattleStruct->monToSwitchIntoId[gActiveBattler];
+        *(gBattleStruct->AI_monToSwitchIntoId + gActiveBattler) = PARTY_SIZE;
+        *(gBattleStruct->monToSwitchIntoId + gActiveBattler) = chosenMonId;
     }
     BtlController_EmitChosenMonReturnValue(BUFFER_B, chosenMonId, NULL);
     PlayerPartnerBufferExecCompleted(battler);
