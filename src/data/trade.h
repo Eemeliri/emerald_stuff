6e7e3652dca2795620ff9619dbaee9aaa986b0f5
Eemeliri <eemeli.rintanen@gmail.com>
2023-12-06 16:21:41 +0200

    pawmi trade in rustboro

--- src/data/trade.h
+++ src/data/trade.h
@@ -984,21 +984,21 @@ static const union AffineAnimCmd *const sAffineAnims_CrossingMonPics[] =
 
 static const struct InGameTrade sIngameTrades[] =
 {
-    [INGAME_TRADE_SEEDOT] =
+    [INGAME_TRADE_PAWMI] =
     {
-        .nickname = _("Dots"),
-        .species = SPECIES_SEEDOT,
-        .ivs = {5, 4, 5, 4, 4, 4},
-        .abilityNum = 1,
+        .nickname = _("Bolts"),
+        .species = SPECIES_PAWMI,
+        .ivs = {15, 14, 15, 14, 15, 14},
+        .abilityNum = 2,
         .otId = 38726,
         .conditions = {30, 5, 5, 5, 5},
         .personality = 0x84,
-        .heldItem = ITEM_CHESTO_BERRY,
+        .heldItem = ITEM_MACHO_BRACE,
         .mailNum = -1,
         .otName = _("Kobe"),
         .otGender = MALE, 
         .sheen = 10,
-        .requestedSpecies = SPECIES_RALTS
+        .requestedSpecies = SPECIES_RHYHORN
     },
     [INGAME_TRADE_PLUSLE] =
     {
