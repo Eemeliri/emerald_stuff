a4339ced203a0da444dd1a7e88da2727786ae4a8
DizzyEggg <jajkodizzy@wp.pl>
2023-12-18 16:02:07 +0100

    Fix Ice Face ignoring move effects (#3755)
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -9157,14 +9157,8 @@ BattleScript_SoundproofProtected::
 	goto BattleScript_MoveEnd
 
 BattleScript_IceFaceNullsDamage::
-	attackstring
-	attackanimation
-	waitanimation
-	effectivenesssound
-	hitanimation BS_TARGET
-	waitstate
 	call BattleScript_TargetFormChangeWithString
-	goto BattleScript_MoveEnd
+	return
 
 BattleScript_DazzlingProtected::
 	attackstring
