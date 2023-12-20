4919d8d290337c8f54777ae8b5443974f75719df
DizzyEggg <jajkodizzy@wp.pl>
2023-12-20 15:26:28 +0100

    Ally Switch (#3533)
    * ally switch move animation
    
    * Ally Switch anim done
    
    * ally switch test and improve animation
    
    * derp
    
    * add ally switch known failing test for ally targeting moves
    
    * moves which targetted ally fail after ally switch
    
    * ally switch works like protect
    
    ---------
    
    Co-authored-by: root <root@LAPTOP-3SNV7DEQ>
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>

--- data/battle_anim_scripts.s
+++ data/battle_anim_scripts.s
@@ -5634,6 +5634,11 @@ Move_QUICK_GUARD:
 	end
 
 Move_ALLY_SWITCH:
+	call SetPsychicBackground
+	createvisualtask AnimTask_AllySwitchAttacker, 2
+	createvisualtask AnimTask_AllySwitchPartner, 2
+	call DoubleTeamAnimRet
+	call UnsetPsychicBg
 	end
 
 Move_SCALD:
@@ -19357,9 +19362,8 @@ Move_TELEPORT:
 	call UnsetPsychicBg
 	waitforvisualfinish
 	end
-
-Move_DOUBLE_TEAM:
-	createvisualtask AnimTask_DoubleTeam, 2
+	
+DoubleTeamAnimRet:
 	setalpha 12, 8
 	monbg ANIM_ATK_PARTNER
 	playsewithpan SE_M_DOUBLE_TEAM, SOUND_PAN_ATTACKER
@@ -19383,6 +19387,11 @@ Move_DOUBLE_TEAM:
 	clearmonbg ANIM_ATK_PARTNER
 	blendoff
 	delay 1
+	return
+
+Move_DOUBLE_TEAM:
+	createvisualtask AnimTask_DoubleTeam, 2
+	call DoubleTeamAnimRet
 	end
 
 Move_MINIMIZE:
