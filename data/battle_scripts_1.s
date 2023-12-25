9fae7e934953918f4a279799ac415f811a5fe29a
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2023-12-25 18:28:16 +0100

    Adds ability Embody Aspect + minor fix to Hospitality (#3821)
    * Adds ability Embody Aspect + minor fix to Hospitality
    
    * comment out failing tests related to neutralizing gas
    
    * fixes neutralizing gas bug
    
    * leftover

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -9320,6 +9320,7 @@ BattleScript_BattlerAbilityStatRaiseOnSwitchIn::
 	waitanimation
 	printstring STRINGID_BATTLERABILITYRAISEDSTAT
 	waitmessage B_WAIT_TIME_LONG
+	copybyte gBattlerAttacker, sSAVED_BATTLER
 	end3
 
 BattleScript_ScriptingAbilityStatRaise::
