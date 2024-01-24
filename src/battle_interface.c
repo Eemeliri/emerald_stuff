7cc5edd70fa7fc80f2b66a1d60f21879b489660e
Eemeliri <eemeli.rintanen@gmail.com>
2024-01-24 18:30:39 +0200

    new ability popup design by nicoswag

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -3165,7 +3165,7 @@ static void PrintBattlerOnAbilityPopUp(u8 battlerId, u8 spriteId1, u8 spriteId2)
                         (void*)(OBJ_VRAM0) + (gSprites[spriteId2].oam.tileNum * 32),
                         5, 12,
                         0,
-                        2, 7, 1);
+                        8, 7, 1);
 }
 
 static void PrintAbilityOnAbilityPopUp(u32 ability, u8 spriteId1, u8 spriteId2)
@@ -3175,7 +3175,7 @@ static void PrintAbilityOnAbilityPopUp(u32 ability, u8 spriteId1, u8 spriteId2)
                         (void*)(OBJ_VRAM0) + (gSprites[spriteId2].oam.tileNum * 32) + 256,
                         5, 12,
                         4,
-                        7, 9, 1);
+                        6, 9, 1);
 }
 
 #define PIXEL_COORDS_TO_OFFSET(x, y)(            \
